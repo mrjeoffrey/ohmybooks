5444a71c2440b535f12cab2f1a1ec17067d7701f
Jane Tiglao <jane.b.tiglao@gmail.com>
2022-04-04 18:07:37 -0700

    books & book-genre tables created

--- db/schema.sql
+++ db/schema.sql
@@ -4,5 +4,27 @@ CREATE DATABASE books_db;
 
 USE books_db;
 
+CREATE TABLE books (
+    id  INT NOT NULL,
+    title VARCHAR(30),NOT NULL,
+    description TEXT,
+    isbn VARCHAR(30), NOT NULL,
+    genre VARCHAR(30), NOT NULL,
+    author VARCHAR(30), NOT NULL,
+    publication VARCHAR(30), NOT NULL,
+    PRIMARY KEY (id)
+);
 
+CREATE TABLE book_genre (
+    id INT NOT NULL,
+    genre_id INT NOT NULL,
+    title TEXT, 
+    description TEXT,
+    isbn VARCHAR(30) NOT NULL,
+    author VARCHAR(30), NOT NULL,
+    publication VARCHAR(30), NOT NULL,
+    FOREIGN KEY (genre_id)
+    REFERENCES books(id)
+    ON DELETE SET NULL
+);
 
